f78aad83878d62b07ca18b5b9cb84f45be60d981
rafael <rafaelcoelho2711@gmail.com>
2024-01-04 18:31:37 -0300

    atualizaçao da url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "https://food-explorer-api-a3j9.onrender.com",
+  baseURL: 'https://food-explorer-api-a3j9.onrender.com',
   withCredentials: true
 })
